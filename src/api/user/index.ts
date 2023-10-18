9103198f24f62b0230f65074431dd6d7e51f1ea2
winx <1577648502@qq.com>
2023-10-18 20:40:04 +0800

    update 修改头像不实时跟新和获取融资人头像问题

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,4 +1,4 @@
-import { request } from "@/utils/service"
+import {request} from "@/utils/service"
 import type * as Login from "./types/login"
 import {LoginResponseData} from "./types/login";
 
@@ -11,11 +11,11 @@ export function getLoginCodeApi() {
 }
 
 /** 登录并返回 Token */
-export function uploadApi(params) {
+export function uploadApi(params:any) {
   const formData = new FormData()
   formData.append('file', params.file)
   // axios的二次封装
-  return  request({
+  return request({
     url: '/file/upload',
     method: 'post',
     headers: {
@@ -24,6 +24,7 @@ export function uploadApi(params) {
     data: formData
   })
 }
+
 /** 注册 */
 export function modifyUserApi(data: Login.LoginRequestData) {
   return request<Login.LoginResponseData>({
@@ -52,13 +53,22 @@ export function getUserInfoApi() {
 //   })
 // }
 /** 查 */
-export function getUserDataApi(data,params: Login.LoginResponseData) {
+export function getUserDataApi(data:any, params: Login.LoginResponseData) {
   return request<Login.LoginResponseData>({
     url: `user/getPageUser?size=${params.size}&current=${params.current}`,
     method: "post", // 使用POST请求
     data // 将JSON数据放在请求体中
   })
 }
+
+/** 查头像 */
+export function getUserImgApi(userName: Login.LoginResponseData) {
+  return request<Login.LoginResponseData>({
+    url: `user/getUserImg?userName=` + userName,
+    method: "get", // 使用POST请求
+  })
+}
+
 /** 改 */
 export function updateUserDataApi(data: Login.UpdateUserData) {
   return request({
